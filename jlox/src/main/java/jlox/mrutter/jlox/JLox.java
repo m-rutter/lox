3d87c40dead12f0b112b58d8c2a0bf265e03fa57
Michael Rutter <michael.rutter@protonmail.com>
2022-05-21 23:27:40 +0100

    [jlox] implmented some of the token matching

--- jlox/src/main/java/jlox/mrutter/jlox/JLox.java
+++ jlox/src/main/java/jlox/mrutter/jlox/JLox.java
@@ -6,10 +6,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-
-import jlox.mrutter.jlox.Token;
-import jlox.mrutter.jlox.Scanner;
 
 /**
  * JLox - Java implementation of Lox
@@ -32,7 +28,7 @@ public class JLox {
     }
 
     private static void runFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        var bytes = Files.readAllBytes(Paths.get(path));
 
         run(new String(bytes, Charset.defaultCharset()));
 
@@ -42,12 +38,12 @@ public class JLox {
     }
 
     private static void runPrompt() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
+        var input = new InputStreamReader(System.in);
+        var reader = new BufferedReader(input);
 
         for (;;) {
             System.out.print("> ");
-            String line = reader.readLine();
+            var line = reader.readLine();
             if (line == null) {
                 break;
             }
@@ -57,8 +53,8 @@ public class JLox {
     }
 
     private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
+        var scanner = new Scanner(source);
+        var tokens = scanner.scanTokens();
 
         for (Token token : tokens) {
             System.out.println(token);
